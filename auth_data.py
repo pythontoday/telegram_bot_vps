ddf71e6bc5e3925c14765495e2b6768ece26aa8d
pythontoday <pythontodaynet@gmail.com>
2021-02-07 17:12:10 +0700

    Initial commit

---
+++ auth_data.py
@@ -0,0 +1,1 @@
+token = "your_token"
---
+++ main.py
@@ -0,0 +1,46 @@
+import requests
+from datetime import datetime
+import telebot
+from auth_data import token
+
+
+def get_data():
+    req = requests.get("https://yobit.net/api/3/ticker/btc_usd")
+    response = req.json()
+    sell_price = response["btc_usd"]["sell"]
+    print(f"{datetime.now().strftime('%Y-%m-%d %H:%M')}\nSell BTC price: {sell_price}")
+
+
+def telegram_bot(token):
+    bot = telebot.TeleBot(token)
+
+    @bot.message_handler(commands=["start"])
+    def start_message(message):
+        bot.send_message(message.chat.id, "Hello friend! Write the 'price' to find out the cost of BTC!")
+
+    @bot.message_handler(content_types=["text"])
+    def send_text(message):
+        if message.text.lower() == "price":
+            try:
+                req = requests.get("https://yobit.net/api/3/ticker/btc_usd")
+                response = req.json()
+                sell_price = response["btc_usd"]["sell"]
+                bot.send_message(
+                    message.chat.id,
+                    f"{datetime.now().strftime('%Y-%m-%d %H:%M')}\nSell BTC price: {sell_price}"
+                )
+            except Exception as ex:
+                print(ex)
+                bot.send_message(
+                    message.chat.id,
+                    "Damn...Something was wrong..."
+                )
+        else:
+            bot.send_message(message.chat.id, "Whaaat??? Check the command dude!")
+
+    bot.polling()
+
+
+if __name__ == '__main__':
+    # get_data()
+    telegram_bot(token)
